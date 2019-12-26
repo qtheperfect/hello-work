98beb8af4250500188e60274544f5c5f7b338b61
fulgenssequar <guolh2013@hotmail.com>
2019-12-26 10:41:13 +0800

    bracket spelling

--- quine/python-quine.py
+++ quine/python-quine.py
@@ -4,7 +4,7 @@ l=[
     ,'q=str([str(0), str(0)])[1] # The single quote'
     ,'l=['
     ,']'
-    ,'for a in l[1:3]: # print to left braket, then insert List items'
+    ,'for a in l[1:3]: # print to left bracket, then insert List items'
     ,'    print(a)'
     ,'print(l[0]+q+l[0]+q)'
     ,'for a in l[1:]:'
@@ -12,7 +12,7 @@ l=[
     ,'for a in l[3:]:'
     ,'    print(a)'
 ]
-for a in l[1:3]: # print to left braket, then insert List items
+for a in l[1:3]: # print to left bracket, then insert List items
     print(a)
 print(l[0]+q+l[0]+q)
 for a in l[1:]:
