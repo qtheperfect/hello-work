8ec7264cee5569e6906e9b4b2a3cb3b234010ea8
Linghuei Guo <guolh2013@hotmail.com>
2026-09-04 16:23:00 +0800

    Add RSA 260 cracker implementation in Python

---
+++ src/numeric/rsa260cracker.py
@@ -0,0 +1,103 @@
+import sys
+
+rsa260 = 22112825529529666435281085255026230927612089502470015394413748319128822941402001986512729726569746599085900330031400051170742204560859276357953757185954298838958709229238491006703034124620545784566413664540684214361293017694020846391065875914794251435144458199
+rf1 = 4397328654844826923795068102505872571721883526553349659561256924505973939597593482272505698004801207988043088656411102133523080581
+rf2 = rsa260 // rf1
+
+def abs(n: int) -> int:
+    return n if n >= 0 else -n
+
+def gcd(x: int, y: int) -> int:
+    while y != 0:
+        x, y = y, x%y
+    return x
+
+def modpow(base: int, expn: int, modl: int) -> int:
+    result = 1
+    while expn != 0:
+        if expn % 2 == 1:
+            result = (result * base) % modl
+        expn = expn // 2
+        base = (base * base) % modl
+    return result
+
+def isprime(n: int) -> int:
+    seed = [51, 52111, 8817]
+    for s in seed:
+        if modpow(s, n - 1, n) != 1:
+            return False
+    return True
+
+def findPrime(init = 1000):
+    init = init - init % 4 + 3
+    while not isprime(init):
+        init = init + 4
+    return init
+
+p1 = findPrime(100000)
+p2 = findPrime(p1 + 1)
+ntest = p1 * p2
+
+attemptCount = 0
+def rho(n: int, c: int, x0: int) -> int:
+    x = x0
+    y = x0
+    while True:
+        global attemptCount
+        
+        x = (x * x + c) % n
+        y = (y * y + c) % n
+        y = (y * y + c) % n
+
+        dxy = abs(x - y)
+        if dxy == 0:
+            return 1
+        elif gcd(dxy, n) != 1:
+            return gcd(dxy, n)
+        else:
+            attemptCount += 1
+            if  attemptCount % 1000 == 0:
+                print(f"failed attempts: {attemptCount}", end = "\r")
+
+def test():
+    nfactor = rho(n = ntest, c = 1531, x0 = 151)
+    print(f"\n result:  {nfactor} | {ntest}")
+    print(  f" test  :  {nfactor} * {ntest // nfactor} =  {nfactor * (ntest // nfactor)}")
+    return nfactor
+
+result = 1
+def run():
+    global result
+    result = rho(rsa260, 512341, 5121)
+    print(f"\n    Factor of {rsa260} is:\n {result}")
+    return result
+
+def verify():
+    prd = rf1 * rf2
+    print(f"""
+    rsa260  = {rsa260}
+    factor1 = {rf1}
+    factor2 = {rf2}
+    f1 * f2 = {prd}
+    result:   {prd == rsa260}
+    """)
+    
+    
+
+for c in sys.argv:
+    if c == "run":
+        run()
+        break 
+    elif c == "test":
+        test()
+        break
+    elif c == "verify":
+        verify()
+        break
+        
+
+
+        
+        
+        
+    
